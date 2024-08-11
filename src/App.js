590f5c4576a03e91568a0764f3fe8495db93cac9
DAniel <asogwadanny@gmail.com>
2024-08-11 02:11:28 +0100

    chijioke is good

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import './App.css';
 import Header from './Header';
